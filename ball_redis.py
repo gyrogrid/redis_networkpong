bbbc504b66baa77a0353207f4c58fbae920bedc0
siddharth <gyrogrid@gmail.com>
2015-12-24 09:40:21 +0530

    redis networkpong

---
+++ ball_redis.py
@@ -0,0 +1,74 @@
+import time
+import redis
+import rpyc
+import pygame
+from pygame.locals import *
+
+r = redis.Redis(host = 'localhost')
+
+r.set('ball_x', 400)
+r.set('ball_y', 300)
+r.set('ball_dx', 10)
+r.set('ball_dy', 15)
+r.set('pad1_x', 0)
+r.set('pad1_y', 300)
+r.set('pad2_x', 790)
+r.set('pad2_y', 300)
+r.set('score1', 0)
+r.set('score2', 0)
+
+
+def moveball():
+	
+	if int(r.get('ball_y')) < 0 or int(r.get('ball_y')) > 600:
+		newdy = int(r.get('ball_dy')) * -1
+		r.set('ball_dy', newdy)
+		
+	if int(r.get('ball_x')) <= 0:
+		if int(r.get('pad1_y')) <= int(r.get('ball_y')) <= int(r.get('pad1_y'))+200:
+			newdx = int(r.get('ball_dx')) * -1
+			r.set('ball_dx',newdx)
+			print(r.get('ball_dx'), newdx)
+		else:
+			r.incr('score2')
+			r.set('ball_x', 400)
+			r.set('ball_y', 300)
+
+	if int(r.get('ball_x')) >= 790:
+		if int(r.get('pad2_y')) <= int(r.get('ball_y')) <= int(r.get('pad2_y'))+200:
+			newdx = int(r.get('ball_dx')) * -1
+			r.set('ball_dx',newdx)
+			print(r.get('ball_dx'), newdx)
+		else:
+			r.incr('score1')
+			r.set('ball_x', 400)
+			r.set('ball_y', 300)
+	
+
+	r.incrby('ball_x', int(r.get('ball_dx')))
+	r.incrby('ball_y', int(r.get('ball_dy')))
+
+	time.sleep(.05)
+	print(r.get('ball_x'), r.get('ball_y'), r.get('ball_dx'), r.get('ball_dy'), r.get('pad1_x'), r.get('pad1_y'), r.get('pad2_x'), r.get('pad2_y'), r.get('score1'), r.get('score2'))
+
+def movepad(direction, player):
+	if player == 1:
+		if direction == 'up':
+			r.incrby('pad1_y', -10)
+		else:
+			r.incrby('pad1_y', 10)
+
+	if player == 2:
+		if direction == 'up':
+			r.incrby('pad2_y', -10)
+		else:
+			r.incrby('pad2_y', 10)
+
+
+def main():
+	while 1:
+
+		moveball()
+
+if __name__ == '__main__':
+	main()
---
+++ client_1.py
@@ -0,0 +1,88 @@
+import redis
+import pygame
+from pygame.locals import *
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+
+class Ball(pygame.sprite.Sprite):
+	def __init__(self):
+		#initialise the sprite
+		pygame.sprite.Sprite.__init__(self)
+		
+		#create the image using the surface function, color it blue
+		self.image = pygame.Surface([10, 10])
+		self.image.fill(BLUE)
+
+		#assign a rectangle, which is what we will use to move the image
+		self.rect = self.image.get_rect()
+
+
+class Pad(pygame.sprite.Sprite):
+	def __init__(self):
+		
+		#initialise the sprite that represents the pad.
+		pygame.sprite.Sprite.__init__(self)
+		
+		#create the image using the surface function, color it
+		self.image = pygame.Surface([10, 200])
+		self.image.fill(BLUE)
+		self.rect = self.image.get_rect()
+
+def main():
+	pygame.init()
+	size = [800, 600]
+	screen = pygame.display.set_mode(size)
+	pygame.display.set_caption("Pong Yourself")
+	pygame.mouse.set_visible(0)
+	background = pygame.Surface(screen.get_size())
+	font = pygame.font.Font(None, 30)
+
+	ball = Ball()
+	pad1 = Pad()
+	pad2 = Pad()
+
+	balls = pygame.sprite.Group()
+	balls.add(ball)
+
+	allsprites = pygame.sprite.RenderPlain((balls, pad1, pad2))
+	clock = pygame.time.Clock()
+
+	r = redis.Redis(host = '104.236.206.128')
+
+	while 1:
+		background.fill(BLACK)
+		text = font.render("Player 1: "+str(r.get('score1')), True, WHITE)
+		background.blit(text, (150,0))
+		text = font.render("Player 2: "+str(r.get('score2')), True, WHITE)
+		background.blit(text, (450,0))
+
+		#Event/Input handler.
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				return
+			elif event.type == KEYDOWN and event.key == K_ESCAPE:
+				return
+			elif event.type == KEYDOWN and event.key == K_UP:
+				r.incrby('pad1_y', -10)
+			elif event.type == KEYDOWN and event.key == K_DOWN:
+				r.incrby('pad1_y', 10)
+
+		ball.rect.x = int(r.get('ball_x'))
+		ball.rect.y = int(r.get('ball_y'))
+		pad1.rect.x = int(r.get('pad1_x'))
+		pad1.rect.y = int(r.get('pad1_y'))
+		pad2.rect.x = int(r.get('pad2_x'))
+		pad2.rect.y = int(r.get('pad2_y'))
+
+		#print(ball.rect.x)
+		#print(ball.rect.y)
+		clock.tick(10)
+		screen.blit(background, (0, 0))
+		allsprites.draw(screen)
+		pygame.display.flip()
+
+if __name__ == '__main__':
+	main()
